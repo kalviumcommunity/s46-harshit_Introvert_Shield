9351b0a4ef8c3e82bd572b1db7d923d135edbb27
Harshitchandel20 <harsitchandel100@.gmail.com>
2024-02-08 14:30:52 +0530

    created react app

---
+++ Front-end/client/Introvert_Shield/src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <>
+    <div>
+      <h1>Welcome to Intovert Shield</h1>
+      <h3>A Place made for and made by Intovert People.</h3>
+    </div>
+    </>
+  )
+}
+
+export default Home
