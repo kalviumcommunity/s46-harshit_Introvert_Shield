7ad3134f8b2fa9e640832d12008b490b03c77974
Harshitchandel20 <harsitchandel100@.gmail.com>
2024-04-05 11:57:59 +0530

    worked on update & delete

---
+++ client/Introvert_Shield/src/components/UpdatePlace.jsx
@@ -0,0 +1,144 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+function UpdatePlace() {
+  const { id } = useParams();
+
+  const [place, setPlace] = useState("");
+  const [image, setImage] = useState("");
+  const [density, setDensity] = useState("");
+  const [wifi, setWifi] = useState("");
+  const [comfort, setComfort] = useState("");
+
+  const nav = useNavigate()
+
+  useEffect(() => {
+    axios.get(import.meta.env.VITE_API_URL + id).then((data) => {
+      console.log(data);
+      setComfort(data.data.Seating_Comfort);
+      setDensity(data.data.Crowd_Density);
+      setImage(data.data.Image_Link);
+      setPlace(data.data.Place_Type);
+      setWifi(data.data.WiFi_Availability);
+    });
+  }, [id]);
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    axios
+      .patch(   
+        import.meta.env.VITE_API_URL + id,
+        {
+            Place_Type: place,
+            Image_Link: image,
+            Crowd_Density: density,
+            Seating_Comfort: comfort,
+            Wifi_Availability: wifi,
+        }
+      )
+      .then(() => {
+          nav("/home");
+      })
+      .catch((err) => {
+        console.error(err)
+      });
+  };
+
+
+  return (
+    <>
+      <div>
+        <div className="introvert-container">
+          <div className="introvert-modal">
+            <div className="introvert-modal__header">
+              <span className="introvert-modal__title">Update Place</span>
+            </div>
+
+            <div className="introvert-modal__body">
+              <div className="introvert-input">
+                <label className="introvert-input__label">Place Name</label>
+                <input
+                  className="introvert-input__field"
+                  type="text"
+                  value={place}
+                  onChange={(e) => setPlace(e.target.value)}
+                />
+                <p className="introvert-input__description">
+                  The title must contain a maximum of 3 characters
+                </p>
+              </div>
+
+              <div className="introvert-input">
+                <label className="introvert-input__label">Image Link</label>
+                <input
+                  className="introvert-input__field"
+                  type="text"
+                  value={image}
+                  onChange={(e) => setImage(e.target.value)}
+                />
+                <p className="introvert-input__description">
+                  Please copy image link and paste it here
+                </p>
+              </div>
+
+              <div className="introvert-input">
+                <label className="introvert-input__label">Crowd Density</label>
+                <input
+                  className="introvert-input__field"
+                  type="text"
+                  value={density}
+                  onChange={(e) => setDensity(e.target.value)}
+                />
+                <p className="introvert-input__description">
+                  Mention the Density of people normally present there E.g High
+                  Density.
+                </p>
+              </div>
+
+              <div className="introvert-input">
+                <label className="introvert-input__label">
+                  Seating Comfort
+                </label>
+                <input
+                  className="introvert-input__field"
+                  type="text"
+                  value={comfort}
+                  onChange={(e) => setComfort(e.target.value)}
+                />
+                <p className="introvert-input__description">
+                  How much comfortable is seating place.
+                </p>
+              </div>
+
+              <div className="introvert-input">
+                <label className="introvert-input__label">
+                  Wifi Availability
+                </label>
+                <input
+                  className="introvert-input__field introvert-input__field--textarea"
+                  value={wifi}
+                  onChange={(e) => setWifi(e.target.value)}
+                />
+                <p className="introvert-input__description">
+                  Please mention if the Wi-Fi Available or not.
+                </p>
+              </div>
+            </div>
+
+            <div className="introvert-modal__footer">
+              <button
+                className="introvert-button create"
+                  onClick={handleUpdate}
+              >
+                Update Place
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UpdatePlace;
